351d461b8539d57b3ee97e6f09bd738730eb6981
Ryan Attard <ryanattard@gmail.com>
2022-10-05 12:28:19 +0200

    created projects + started adding models. no references yet

---
+++ BANEnterpriseProgramming2022/Domain/Models/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Models
+{
+    //Classes within the Domain Models, will be used to model/shape the database within a
+    //code-first approach
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public double Price { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
