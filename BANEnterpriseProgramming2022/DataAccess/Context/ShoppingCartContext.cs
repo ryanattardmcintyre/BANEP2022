3fecb7718e6f6b7a088a519ca0ac711ae7e2cff4
Ryan Attard <ryanattard@gmail.com>
2022-10-10 10:43:26 +0200

    References addes successfully NuGet Packages added successfully

---
+++ BANEnterpriseProgramming2022/DataAccess/Context/ShoppingCartContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Context
+{
+    /*public class ShoppingCartContext : IdentityDbContext
+    {
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
+           : base(options)
+        {
+        }
+    }*/
+}
